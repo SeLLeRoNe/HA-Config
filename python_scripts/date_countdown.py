0873934d619c055321b6d6edf69790ac600482cb
Andrea Iannucci <sellerone@sellerone.it>
2020-02-20 10:53:18 +0100

    Updated Custom Components, Cards, Themes and Scripts

--- python_scripts/date_countdown.py
+++ python_scripts/date_countdown.py
@@ -12,38 +12,40 @@ dateMonth = int(dateSplit[1])
 dateYear =  int(dateSplit[2])
 date = datetime.date(dateYear , dateMonth , dateDay)
 
-thisYear = int(today.year)
-nextOccur = datetime.date(thisYear , dateMonth , dateDay)
+nextOccurYear = int(today.year)
+nextOccur = datetime.date(nextOccurYear , dateMonth , dateDay)
+
+if nextOccur < date:
+  # date must be the first occurrence
+  nextOccur = date
+
+if nextOccur < today:
+  # if event has passed this year, nextOccur is next year
+  nextOccurYear = nextOccurYear + 1
+  nextOccur = datetime.date(nextOccurYear, dateMonth, dateDay)
 
 numberOfDays = 0
-years = dateYear - thisYear
+years = nextOccurYear - dateYear
 
-# if the year is "next year" or further
-if years > 0:
-  nextOccur = datetime.date(dateYear , dateMonth , dateDay)
-  numberOfDays = (nextOccur - today).days
-else:
-  
-  numberOfDays = (nextOccur - today).days
-  # if event has passed this year
-  if numberOfDays < 0:
-    nextYear = thisYear + 1
-    nextOccur = datetime.date(nextYear , dateMonth , dateDay)
-    numberOfDays = (nextOccur - today).days
-    years = years + 1
+if years < 0:
+  # if years is negative, then date is more than 365 days away
+  # nextOccur will be the first occurrence
+  years = 0
 
-friendly_name = ''
+numberOfDays = (nextOccur - today).days
+
+defaultFriendlyName = ''
 
 if type.lower() == 'birthday':
-  friendly_name = "{}'s {}".format(name , type)
+  defaultFriendlyName = "{}'s {}".format(name , type)
 else:
-  friendly_name = "{} {}".format(name , type)
+  defaultFriendlyName = "{} {}".format(name , type)
 
 hass.states.set(sensorName , numberOfDays ,
   {
-    "icon" : "mdi:calendar-star" ,
+    "icon" : data.get("icon", "mdi:calendar-star"),
     "unit_of_measurement" : "days" ,
-    "friendly_name" : friendly_name,
+    "friendly_name" : data.get('friendly_name', defaultFriendlyName),
     "nextoccur" : "{}/{}/{}".format(nextOccur.day , nextOccur.month , nextOccur.year) ,
     "years" : years
   }
