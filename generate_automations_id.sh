9fb1ab5ab6ba919ec21e1606102560f4bf50271c
Andrea Iannucci <sellerone@sellerone.it>
2021-01-29 13:09:49 +0100

    Added code to remove existing id before creating a new one, disabled for now

--- generate_automations_id.sh
+++ generate_automations_id.sh
@@ -3,8 +3,9 @@
 cd /home/ha/.homeassistant/automations/
 for FILE in `find ./ -name *.yaml`
 	do
+#	sed -i '/^id: /d' $new_file
 	if [ "`head -2 $FILE | grep \"id: \"`" = "" ]; then
-		RANDOM_ID=`</dev/urandom tr -dc 0-9 | head -c13`
+		RANDOM_ID=`</dev/urandom tr -dc 0-9 | head -c15`
 		sed -i "2 i id: '$RANDOM_ID'" $FILE
 	fi
 done
