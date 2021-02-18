89b83c8be54d7a6a84fc1c5a73ac2f0d6e291ac5
Andrea Iannucci <sellerone@sellerone.it>
2021-02-18 12:17:54 +0100

    Updated Custom Components, Cards, Themes and Scripts

--- www/community/zigbee2mqtt_networkmap/settings.js
+++ www/community/zigbee2mqtt_networkmap/settings.js
@@ -1,2 +1,2 @@
-var webhook_trigger_update_id = '688a1938175dd21fbc461fa6c3840a74e382db6b41dbd26e4dd40577ae47498e';
-var webhook_check_update_id = '082129cc8e50700c4cd9e7935313e328c3ebd30a9afc0224a748a9f9a57f2f6b';
+var webhook_trigger_update_id = '0b164656c01036fd2f07eb2072506439779e3225fcf21f0e50e44033cd3c1ee9';
+var webhook_check_update_id = 'e8592f14fee2f2964437678fc0cba54256e0aa22eae59d5bad88d20673bece9b';
