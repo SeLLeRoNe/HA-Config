2e41ba2d506bd8e34d660edd39185658a6a1b51a
Andrea Iannucci <sellerone@sellerone.it>
2021-02-23 15:46:29 +0100

    Improved ID Generator

--- generate_automations_id.sh
+++ generate_automations_id.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+if ["$1" = "clean" ]; then
+	NEW_IDS=1
+else
+	NEW_IDS=0
+fi
+
 cd /home/ha/.homeassistant/automations/
 for FILE in `find ./ -name *.yaml`
 	do
-#	sed -i '/^id: /d' $FILE
+	if [ $NEW_IDS -eq 1 ]; then
+		sed -i '/^id: /d' $FILE
+	fi
 	RANDOM_ID=`</dev/urandom tr -dc 0-9 | head -c15`
 	if [ "`head -2 $FILE | grep \"id: \"`" = "" ]; then
 		sed -i "2 i id: '$RANDOM_ID'" $FILE
